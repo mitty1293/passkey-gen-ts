6ef179e88147325c692d720e9bfbaa650e62c532
mitty1293 <k.fmichi@gmail.com>
2021-03-08 21:02:09 +0900

    fix

---
+++ main.js
@@ -0,0 +1,18 @@
+// DOM
+const gen_btn = document.getElementById("gen_btn");
+const copy_btn = document.getElementById("copy_btn");
+const input_txt = document.getElementById("input_data");
+const output_txt = document.getElementById("output_data");
+
+function generate(){
+    output_txt.value = input_txt.value;
+}
+
+function cp(){
+    output_txt.select();
+    document.execCommand("Copy");
+}
+
+// eventlistener
+gen_btn.addEventListener('click', generate, false);
+copy_btn.addEventListener('click', cp, false);
