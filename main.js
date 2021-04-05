2369849dfabee04df888f41418d55ba4acccb83b
mitty1293 <k.fmichi@gmail.com>
2021-04-05 20:07:10 +0900

    fix

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import {generate_hash} from './sha256.js';
+//import {generate_hash} from './sha256.js';
+const hash = require('./compute_hash');
 
 // DOM
 const gen_btn = document.getElementById("gen_btn");
@@ -8,7 +9,8 @@ const output_txt = document.getElementById("output_data");
 
 // eventlistener
 gen_btn.addEventListener('click', () => {
-    output_txt.value = generate_hash(input_txt.value);
+    //output_txt.value = generate_hash(input_txt.value);
+    output_txt.value = hash.computeHash(input_txt.value);
 });
 
 copy_btn.addEventListener('click', () => {
